465e9d8aa53ae8824e67216dee7fcc6222a35746
JavaG23 <geof.hoffman@gmail.com>
2019-07-05 09:57:53 -0400

    full upload of Flask-blog

---
+++ flas.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+pip3 install flask
+
+pip3 install Flask-WTF
+
+pip3 install flask-sqlalchemy
+
+
+sudo apt-get install libffi-dev
+
+easy_install flask-bcrypt
+
+pip3 install flask-login
+
+
+
+# to print list of dependencies in virtualenv: 
+#pip freeze --local > requirements.txt
+#
+# to deactivate virtualenv
+#deactivate
+#
+#to remove virtual env:
+#rm -rf <virtual environment file> 
+#
+#
+#
+# to install dependencies from list
+#pip install -r <path>/requirements.txt
+#
+# to launch flask from project directory
+#FLASK_DEBUG=1
+#FLASK_APP=flaskblog.py flask run
+#
+# to add alias to ./bash aliases file
+#echo "alias aliasname='command'" >> ~/.bash_aliases && source ~/.bash_aliases
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+ 
